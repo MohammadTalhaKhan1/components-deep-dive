17b97d0122e2884cf06b036eae46b7d91736a986
Talha <mohammad.talha.khan0@gmail.com>
2019-04-10 01:06:51 +0500

    'first'

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ serverElements = [{type: 'server', name: 'testserver', content: 'just a number' }];
+ onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName ,
+    content: serverData.serverContent
+  });
+}
+
+
+onBlueprintAdded(bluePrint: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'blueprint',
+    name: bluePrint.serverName ,
+    content: bluePrint.serverContent
+  });
+}
+
+
+
+
+}
+
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') bluePrint = new EventEmitter<{ serverName: string, serverContent: string }>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddServer() {
+    this.serverCreated.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  }
+  onAddBlueprint() {
+    this.bluePrint.emit({ serverName: this.newServerName, serverContent: this.newServerContent });
+  }
+
+}
